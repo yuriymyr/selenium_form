eb544cb227a984bb8e61f078dc8df06ce5a42247
yuriymyr <60068368+yuriymyr@users.noreply.github.com>
2020-07-31 09:17:36 +0300

    selenium basic
    selenium basic

--- selenium_basic.py
+++ selenium_basic.py
@@ -4,24 +4,29 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-
 TEST_DATA = {
         'single_input': 'My best message here!!',
         'two_input_first': 333,
         'two_input_second': 444
     }
-
-
-
 driver = webdriver.Chrome()
-driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
-
 if driver.find_element(By.ID, "at-cv-lightbox-close").size != 0:
     driver.find_element(By.ID, "at-cv-lightbox-close").click()
+def test():
+    driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
 
-driver.find_element(By.CSS_SELECTOR, "input#user-message").send_keys(TEST_DATA['single_input'])
-driver.find_element(By.CSS_SELECTOR, "form#get-input button").click()
+    driver.find_element(By.CSS_SELECTOR, "input#user-message").send_keys(TEST_DATA['single_input'])
+    driver.find_element(By.CSS_SELECTOR, "form#get-input button").click()
 
-assert driver.find_element(By.ID, "display").text == TEST_DATA['single_input']
-driver.quit()
+    assert driver.find_element(By.ID, "display").text == TEST_DATA['single_input']
+    driver.quit()
+
+
+def test1():
+    driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
+    driver.find_element(By.CSS_SELECTOR, "#sum1").send_keys(TEST_DATA['two_input_first'])
+    driver.find_element(By.CSS_SELECTOR, "#sum2").send_keys(TEST_DATA['two_input_second'])
+    driver.find_element(By.CSS_SELECTOR, "#gettotal > button").click()
+    assert driver.find_element(By.CSS_SELECTOR, "#displayvalue").text == str(TEST_DATA['two_input_first'] + TEST_DATA['two_input_second'])
+    driver.quit()
+
